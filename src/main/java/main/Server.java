c3d94ceffcc79d110be6c1db6fac348f54c14748
Oz671 <rubinsteinoz5@gmail.com>
2018-09-04 14:27:53 +0300

    Add files via upload

--- src/main/java/main/Server.java
+++ src/main/java/main/Server.java
@@ -14,34 +14,35 @@ import io.vertx.ext.web.*;
 public class Server extends AbstractVerticle {
 	private Router router;
 	/*
-	 * Used TreeMap to make the search to be O(logN) where N is the number of words in the collection
+	 * Used TreeMap to make the search to be O(logN) where N is the number of
+	 * words in the collection
 	 */
 	private TreeMap<Integer, String> valueToWord = new TreeMap<Integer, String>();
 	private TreeMap<String, String> lexicalToWord = new TreeMap<String, String>();
 
 	private void updateCollections(String word) {
-		if(lexicalToWord.containsKey(word))
+		if (lexicalToWord.containsKey(word))
 			return;
-		
+
 		valueToWord.put(calculateValue(word), word);
-		lexicalToWord.put(word,word);
+		lexicalToWord.put(word, word);
 	}
 
 	private Integer calculateValue(String word) {
 		int sum = 0;
-		for(int i=0;i<word.length();i++)
-		{
-			sum += (word.charAt(i)-'a'+1);
+		for (int i = 0; i < word.length(); i++) {
+			sum += (word.charAt(i) - 'a' + 1);
 		}
 		return sum;
 	}
-	
-	private String wrap(String str){
-		return str == null ? "null" : "\""+str+"\"";
+
+	private String wrap(String str) {
+		return str == null ? "null" : "\"" + str + "\"";
 	}
 
 	private String createResponse(String value, String lexical) {
-		return "{\"value\": "+wrap(value)+", \"lexical\": "+wrap(lexical)+"}";
+		return "{\"value\": " + wrap(value) + ", \"lexical\": " + wrap(lexical)
+				+ "}";
 	}
 
 	private String emptyReponse() {
@@ -64,7 +65,8 @@ public class Server extends AbstractVerticle {
 									return;
 								}
 
-								String word = json.getString("text").toLowerCase();
+								String word = json.getString("text")
+										.toLowerCase();
 								if (word.trim().contains(" ")) {
 									response.putHeader("content-type",
 											"text/json").end(emptyReponse());
@@ -102,22 +104,55 @@ public class Server extends AbstractVerticle {
 	private String getLexicalOfWord(String word) {
 		String before = lexicalToWord.floorKey(word);
 		String after = lexicalToWord.ceilingKey(word);
-		return after == null ? before : after;
+
+		if (before == null)
+			return after;
+
+		if (after == null)
+			return before;
+
+		int beforeDistance = calculateLevinsteinDistance(before, word);
+		int afterDistance = calculateLevinsteinDistance(after, word);
+
+		int min = Math.min(beforeDistance, afterDistance);
+
+		return min == beforeDistance ? before : after;
+	}
+
+	private int calculateLevinsteinDistance(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+
+		int[] costs = new int[b.length() + 1];
+		for (int j = 0; j < costs.length; j++)
+			costs[j] = j;
+		for (int i = 1; i <= a.length(); i++) {
+			costs[0] = i;
+			int nw = i - 1;
+			for (int j = 1; j <= b.length(); j++) {
+				int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]),
+						a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
+				nw = costs[j];
+				costs[j] = cj;
+			}
+		}
+		return costs[b.length()];
+
 	}
 
 	private String getValueOfWord(String word) {
 		int value = calculateValue(word);
 		Integer before = valueToWord.floorKey(value);
 		Integer after = valueToWord.ceilingKey(value);
-		if(before == null && after == null)
+		if (before == null && after == null)
 			return null;
 
-		if(before == null && after != null)
+		if (before == null && after != null)
 			return valueToWord.get(after);
-		
-		if(before != null && after == null)
+
+		if (before != null && after == null)
 			return valueToWord.get(before);
-		
+
 		int closest = value - before < after - value ? before : after;
 		return valueToWord.get(closest);
 	}
